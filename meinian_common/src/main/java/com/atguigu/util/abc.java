16ea67a6c58f41fd6b1e44333a5654d9dd8ae931
lhttl666 <1779196455@qq.com>
2022-01-01 16:15:08 +0800

    增加多行Dev

--- meinian_common/src/main/java/com/atguigu/util/abc.java
+++ meinian_common/src/main/java/com/atguigu/util/abc.java
@@ -4,5 +4,10 @@ public class abc {
     public static void main(String[] args) {
         System.out.println("11111111111111111111111master");
         System.out.println("11111111111111111111111Dev");
+        System.out.println("11111111111111111111111Dev");
+        System.out.println("11111111111111111111111Dev");
+        System.out.println("11111111111111111111111Dev");
+        System.out.println("11111111111111111111111Dev");
+        System.out.println("11111111111111111111111Dev");
     }
 }
